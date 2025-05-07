de898c752ac67c3a8e04ca874ff9bf2d60f38e23
Binario Assembler <165540435+binarioassembler@users.noreply.github.com>
2025-05-06 21:45:18 -0500

    nvim actualizado al 06/05/2025
    Nueva configuración para keymaps

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,4 +1,4 @@
-vim.bo.tapstop = 2
+vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 2
--- init.lua
+++ init.lua
@@ -1,2 +1,27 @@
 require("config")
---require("config.lazy") primera configuración
+--require("config.lazy") de la última versión
+vim.api.nvim_create_user_command("Binario", function()
+  local picker = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+  local conf = require("telescope.config").values
+
+  picker.new({}, {
+    prompt_title = "Binario",
+    finder = finders.new_table({
+      results = { "alpha", "beta", "delta", "omega" }
+    }),
+    sorter = conf.generic_sorter(),
+    previewer = false,
+    attach_mappings = function (_, map)
+      map("i", "<cr>", function (promt_bufnr)
+        actions.close(promt_bufnr)
+        local entry = action_state.get_selected_entry()
+        vim.notify(entry[1])
+      end)
+
+      return true
+    end,
+  }):find()
+end, {})
